84044c4528200f241f6217c917485e1cfa4b2a26
Bob Jamieson <bamieson@gmail.com>
2023-01-26 20:31:11 +1100

    - Hero WIP - RandomColour util - Installed SASS and split scss into multiple files - Cleanup & initial file structure

---
+++ components/Hero/Hero.component.tsx
@@ -0,0 +1,17 @@
+import RandomColour from "@/utils/RandomColour"
+
+const Hero = () => {
+    return (
+        <div>
+            <h1>
+                React developer.
+                <br/>
+                Designer.
+                <br/>
+                <RandomColour>Bob.</RandomColour>
+            </h1>
+        </div>
+    )
+}
+
+export default Hero
