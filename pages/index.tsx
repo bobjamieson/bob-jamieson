e33adbe6fce444608cd6104bb44e2d0a75635fe8
Bob Jamieson <bamieson@gmail.com>
2023-10-22 15:11:06 +1100

    - Light code cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,7 @@ import { HomeProps } from '../src/types/index.model'
 import { NextPage } from 'next'
 import { NextSeo } from 'next-seo'
 import { ApolloClient, InMemoryCache } from '@apollo/client'
-import {
-  GET_PAGE_HOME,
-  GET_ALL_CASESTUDIES,
-  GET_HERO,
-  GET_CHATBOX,
-} from '@/graphql/queries'
+import { GET_PAGE_HOME, GET_ALL_CASESTUDIES, GET_HERO } from '@/graphql/queries'
 
 const Home: NextPage<HomeProps> = (props) => {
   return (
@@ -38,25 +33,21 @@ export const getStaticProps = async ({ params }: any) => {
     cache: new InMemoryCache(),
   })
 
-  const [pagehomeResult, caseStudiesResult, heroResult, chatBoxResult] =
-    await Promise.all([
-      client.query({ query: GET_PAGE_HOME }),
-      client.query({ query: GET_ALL_CASESTUDIES }),
-      client.query({ query: GET_HERO }),
-      client.query({ query: GET_CHATBOX }),
-    ])
+  const [pagehomeResult, caseStudiesResult, heroResult] = await Promise.all([
+    client.query({ query: GET_PAGE_HOME }),
+    client.query({ query: GET_ALL_CASESTUDIES }),
+    client.query({ query: GET_HERO }),
+  ])
 
   const pagehomeData = pagehomeResult?.data?.pagehome?.data
   const heroData = heroResult?.data?.hero?.data
   const caseStudyData = caseStudiesResult?.data?.casestudies?.data
-  const chatBoxData = chatBoxResult?.data?.chatbox?.data
 
   return {
     props: {
       pageHome: pagehomeData,
       hero: heroData,
       caseStudies: caseStudyData,
-      chatBox: chatBoxData,
     },
   }
 }
